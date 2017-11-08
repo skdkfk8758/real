05b987298ba6d7f8641b4f84f4da3bdf768c3c31
CarpDm <skdkfk8758@gmail.com>
2017-11-08 12:58:06 +0900

    Second Commit

--- src/main/java/com/your/dream/real/App.java
+++ src/main/java/com/your/dream/real/App.java
@@ -13,8 +13,7 @@ public class App
 
     public static void main( String[] args ) {
     	for(String arg : args) {
-//    		System.out.println("parameter: " + arg);
-			logger("asd");
+			logger(arg);
     	}
     }
 
